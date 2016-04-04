60423f32818ac4bfa76f2c4de89e3712cd1d0543
ollollollo <bonzo.a@gmail.com>
2016-04-05 01:06:33 +0200

    madness

--- src/main/java/com/testing/controller/TestController.java
+++ src/main/java/com/testing/controller/TestController.java
@@ -3,11 +3,7 @@ package com.testing.controller;
 public class TestController {
 
 	//some comment added here
-<<<<<<< HEAD
-
 	// not so briliant comment added.
-=======
-	// editing this file
 	// one more excelent comment added over here
->>>>>>> 77812aaf2bde3e2aadba51be0490a437ddbd41a8
+
 }
